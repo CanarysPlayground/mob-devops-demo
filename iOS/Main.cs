11f99cb1a09812c8a63f1d0573543f898ff0150f
Srivatsa Marichi <srivatsa.m@ecanarys.com>
2022-09-14 15:19:47 +0530

    test

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -9,7 +9,7 @@ namespace UITestDemo.iOS
 {
     public class Application
     {
-        // This is the main entry point of the application
+        // This is the main entry point of the application.
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
